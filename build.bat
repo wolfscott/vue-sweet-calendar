88937fcb664cc91150f22c638202ee2a778c8e91
Wolf Scott <wscott@ioihan.com>
2019-06-29 10:18:21 -0500

    Minor change to the text of the build.bat to make it a little more generic

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@
 echo ----------------[ CHANGE THE VERSION NUMBER ??? ]--------------------
 echo.
 echo Be sure to exit all running apps that are watching this folder
-echo and then re-run the "npm install" on your target.
 echo.
 echo ---------------------------------------------------------------------
 pause
